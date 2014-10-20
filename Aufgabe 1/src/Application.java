03c5048dc98e37639fbf2ab9002137fb415486ef
Tom <Tom@Tom-PC>
2014-10-20 22:17:35 +0200

    Hausaufgaben: Fehler behoben!

--- Aufgabe 1/src/Application.java
+++ Aufgabe 1/src/Application.java
@@ -2,13 +2,13 @@
 public class Application {
 
 	public static void main(String[] args) {
-		Fehler f = new Fehler;
+		Fehler f = new Fehler();
 		f.variable = 10;
-		f.variable2 = 10
+		f.variable2 = 10;
 		f.variable3 = 20;
 		f.methode1();
 		f.methode2();
-		f.methode 3();
+		f.methode3();
 		System.out.println("Alle Fehler sind verschwunden!");
 	}
 
--- Aufgabe 1/src/Fehler.java
+++ Aufgabe 1/src/Fehler.java
@@ -1,20 +1,21 @@
 
 class Fehler {
 
-	Integer variable1
+	Integer variable1;
 	
-	Integer variable 2;
+	Integer variable;
 	
-	String variable3;
+	int variable2;
 	
-	Boolean variable3;
+	int variable3;
 	
-	void methode1 {
+	void methode1() {
 		System.out.println("Methode 1 aufgerufen");
 	}
 	
 	void methode2() {
 		System.out.println("Methode 2 aufgerufen");
+	}	
 	
 	void methode3() {
 		System.out.println("Methode 3 aufgerufen");
